eb8eff3fe912ff5c6dcaf81336882eeeb0f685dc
Sbjn <78596823+Sbjn@users.noreply.github.com>
2024-06-02 14:41:51 +0900

    Update index.js "/"

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -4,88 +4,88 @@ const Backenddomain = "https://mern-backend-6-2024.onrender.com"
 const SumaryApi ={
 
     SignUp:{
-        url: `${Backenddomain}/api/signup`,
+        url: `${Backenddomain}api/signup`,
         method: "post"
     },
 
     SignIn:{
-        url: `${Backenddomain}/api/signin`,
+        url: `${Backenddomain}api/signin`,
         method: "post"
     },
     Current_user:{
-        url: `${Backenddomain}/api/user-detail`,
+        url: `${Backenddomain}api/user-detail`,
         method: "get",
     },
 
     logout_user:{
-        url:`${Backenddomain}/api/userlogout`,
+        url:`${Backenddomain}api/userlogout`,
         method: "get"
     },
 
     allUsers:{
-        url:`${Backenddomain}/api/all-users`,
+        url:`${Backenddomain}api/all-users`,
         method: "get",
     },
 
     updateUser:{
-        url:`${Backenddomain}/api/update-user`,
+        url:`${Backenddomain}api/update-user`,
         method:"post"
     },
 
     upLoadproDuct:{
-        url:`${Backenddomain}/api/upload-product`,
+        url:`${Backenddomain}api/upload-product`,
         method:"post"
     },
 
     Allproduct:{
-        url:`${Backenddomain}/api/get-product`,
+        url:`${Backenddomain}api/get-product`,
         method: "get"
     },
     UpdateProduct:{
-        url:`${Backenddomain}/api/update-product`,
+        url:`${Backenddomain}api/update-product`,
         method:"post"
     },
 
     categoryProduct: {
-        url:`${Backenddomain}/api/get-categoryproduct`,
+        url:`${Backenddomain}api/get-categoryproduct`,
         method:"get"
     },
 
     categoryWiseProduct:{
-        url:`${Backenddomain}/api/category-product`,
+        url:`${Backenddomain}api/category-product`,
         method:"post"
     },
     productDetail:{
-        url:`${Backenddomain}/api/product-detail`,
+        url:`${Backenddomain}api/product-detail`,
         method:"post"
     },
     addToCartProduct : {
-        url : `${Backenddomain}/api/addtocart`,
+        url : `${Backenddomain}api/addtocart`,
         method : 'post'
     },
 
     addToCartProductCount:{
-        url:`${Backenddomain}/api/countAddToCartProduct`,
+        url:`${Backenddomain}api/countAddToCartProduct`,
         method: "get"
     },
     addToCartProductView:{
-        url:`${Backenddomain}/api/view-card-product`,
+        url:`${Backenddomain}api/view-card-product`,
         method:"get"
     },
     updateCartProduct : {
-        url : `${Backenddomain}/api/update-cart-product`,
+        url : `${Backenddomain}api/update-cart-product`,
         method : 'post'
     },
     deleteCartProduct : {
-        url : `${Backenddomain}/api/delete-cart-product`,
+        url : `${Backenddomain}api/delete-cart-product`,
         method : 'post'
     },
     searchProduct : {
-        url : `${Backenddomain}/api/search`,
+        url : `${Backenddomain}api/search`,
         method : 'get'
     },
     filterProduct : {
-        url : `${Backenddomain}/api/filter-product`,
+        url : `${Backenddomain}api/filter-product`,
         method : 'post'
     }
     
